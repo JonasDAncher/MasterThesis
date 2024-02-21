75f30f6fd501f65024c0716089902be01f827d09
ZheChrisMajor <65349713+ZheChrisMajor@users.noreply.github.com>
2024-02-21 15:17:42 +0100

    start of implementation of client

--- Srp/src/Client.rs
+++ Srp/src/Client.rs
@@ -1,37 +1,46 @@
 
-// use hacspec_sha256::*;
-// use hacspec_lib::*;
+use hacspec_sha256::*;
+use hacspec_lib::*;
+use secret_integers::*;
 
-// const a: U32 = 5; //¯\_(ツ)_/¯;  //generate new session secret
+pub type Password = ByteSeq;
+pub type Key = ByteSeq;
+pub type Salt = ByteSeq;
 
-// fn client(salt: &ByteSeq, P: password) {
-//     let g = 5;
-//     let x = hash((salt, P));
-//     let A = g.pow(a); //create new session public key
-//     send(A);
-//     let (B, u) = recieve();
-//     let S = (B - g.pow(x)).pow(a + u * x);
-//     let K = hash(&S);
-//     let M1 = hash((&A, &B, &K));
-//     send(M1);
-//     let M2 = hash((&A, &M1, &K));
-//     let M2recieved = recieve();
-//     if (M2 == M2recieved) {
-//         setSessionkey(K); 
-//     }
-// }
 
-// fn send(m1: _) -> _ {
-//     todo!()
-// }
 
-// fn recieve() -> _ {
-//     todo!()
-// }
 
-// fn setSessionkey(k: _) -> _ { 
-//     todo!()
-// }
+fn client(salt: Salt, P: Password) {
+    let a = U32::classify(5u32); //¯\_(ツ)_/¯;  //generate new session secret
+    let g = 5u32;
+    let x = hash(&hash(&salt).concat(&hash(&P)));
+    let A = g.pow(a.into()); //create new session public key 
+    //FIXME into not sure if information flow is okay, private -> public
+    // todo!();
+    send(&A);
+    let (B, u) = recieve();
+    let S = (B - g.pow(x.)).pow(a + u * x); 
+    let K = hash(&S);
+    let M1 = hash((&A, &B, &K));
+    send(M1);
+    let M2 = hash((&A, &M1, &K));
+    let M2recieved = recieve();
+    if (M2 == M2recieved) {
+        setSessionkey(K); 
+    }
+}
+
+fn send(m1:_) -> () {
+    todo!()
+}
+
+fn recieve() -> (u32,u32) {
+    todo!()
+}
+
+fn setSessionkey(k: _) -> _ { 
+    todo!()
+}
 
 
 /*
