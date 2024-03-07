5e35ec54d15c1f7e7131aa156f042ac941adff0b
ZheChrisMajor <65349713+ZheChrisMajor@users.noreply.github.com>
2024-03-07 14:57:12 +0100

    ahhhhh

--- Diffie-Hellman/src/diffiehellman.rs
+++ Diffie-Hellman/src/diffiehellman.rs
@@ -1,7 +1,6 @@
 use num_bigint::*;
 use hacspec_lib::*;
 
-fn main() {}
 unsigned_public_integer!(DiffInt, 2048);
 
 pub type PK = (DiffInt, DiffInt, DiffInt);
@@ -11,30 +10,34 @@ pub type SessionKey = DiffInt;
 
 
 fn generate_group() -> (BigInt, BigInt) {
-    let mut rng = rand::prelude::thread_rng();
-    let g = rng.gen_bigint(1000).abs();
-    let p = rng.gen_bigint(1000).abs();
-    (g, p)
+    // let mut rng = rand::prelude::thread_rng();
+    // let g = rng.gen_bigint(1000).abs();
+    // let q = rng.gen_bigint(1000).abs();
+    let g = 10000.to_bigint().unwrap();
+    let q = 1156651.to_bigint().unwrap();
+    (g, q)
 }
 
-fn diff_key_gen(g: BigInt, p: BigInt) -> (SK, PK) {
-    let sk = generate_private_key(&p);
-    (sk, calculate_pub_key(DiffInt::from(g), DiffInt::from(p), sk))
+fn diff_key_gen(g: BigInt, q: BigInt) -> (SK, PK) {
+    let sk = generate_private_key(&q);
+    (sk, calculate_pub_key(DiffInt::from(g), DiffInt::from(q), sk))
 }
 
-fn generate_private_key(p: &BigInt) -> SK {
-    let mut rng = rand::prelude::thread_rng();
-    let sk = rng.gen_bigint_range(&One::one(), &p);
-    DiffInt::from(sk)
+fn generate_private_key(q: &BigInt) -> SK {
+    // let mut rng = rand::prelude::thread_rng();
+    // let sk = rng.gen_bigint_range(&One::one(), &q);
+    let sk = 1315000.to_bigint().unwrap();
+    let asd = DiffInt::from(sk) as SK;
+    asd
 }
 
-pub fn calculate_pub_key(g: DiffInt, p: DiffInt, sk: SK) -> PK {
-    (g, p, g.pow_mod(sk, p))
+pub fn calculate_pub_key(g: DiffInt, q: DiffInt, sk: SK) -> PK {
+    (g, q, g.pow_mod(sk, q))
 }
 
 pub fn calculates_shared_key(sk: SK, pk: PK) -> SessionKey {
-    let (_g, p, pz) = pk;
-    let hab = pz.pow_mod(sk, p);
+    let (_g, q, pz) = pk;
+    let hab = pz.pow_mod(sk, q);
     hab
 }
 
@@ -44,9 +47,9 @@ mod tests {
 
     #[test]
     fn diff() {
-        let (g, p) = generate_group();
-        let (ask, apk) = diff_key_gen(g.clone(), p.clone());
-        let (bsk, bpk) = diff_key_gen(g, p);
+        let (g, q) = generate_group();
+        let (ask, apk) = diff_key_gen(g.clone(), q.clone());
+        let (bsk, bpk) = diff_key_gen(g, q);
         let ashared = calculates_shared_key(ask, bpk);
         let bshared = calculates_shared_key(bsk, apk);
         assert_eq!(ashared, bshared);
