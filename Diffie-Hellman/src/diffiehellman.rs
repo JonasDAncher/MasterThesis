1152df559f5646a1487c68296fe46540eaca006f
ZheChrisMajor <65349713+ZheChrisMajor@users.noreply.github.com>
2024-03-08 12:20:30 +0100

    moved

--- Diffie-Hellman/src/diffiehellman.rs
+++ Diffie-Hellman/src/diffiehellman.rs
@@ -9,30 +9,6 @@ pub type KeyPair = (PK, SK);
 pub type SessionKey = DiffInt;
 
 
-fn generate_group() -> (BigUint, BigUint) {
-    let mut rng = rand::prelude::thread_rng();
-    let g = rng.gen_biguint(1000);
-    let q = rng.gen_biguint(1000);
-    // let g = 10000.to_biguint().unwrap();
-    // let q = 1156651.to_biguint().unwrap();
-    (g, q)
-}
-
-fn diff_key_gen(g: BigUint, q: BigUint) -> (SK, PK) {
-    let sk = generate_private_key(&q);
-    let gd = DiffInt::from(g);
-    let qd = DiffInt::from(q.clone());
-    (sk, calculate_pub_key(gd, qd, sk))
-}
-
-fn generate_private_key(q: &BigUint) -> SK {
-    let mut rng = rand::prelude::thread_rng();
-    let sk = rng.gen_biguint_range(&One::one(), &q);
-    // let sk = 1315000.to_biguint().unwrap();
-    let asf = DiffInt::from(sk);
-    asf
-}
-
 pub fn calculate_pub_key(g: DiffInt, q: DiffInt, sk: SK) -> PK {
     (g, q, g.pow_mod(sk, q))
 }
@@ -46,7 +22,7 @@ pub fn calculates_shared_key(sk: SK, pk: PK) -> SessionKey {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn diff() {
         let (g, q) = generate_group();
@@ -56,4 +32,28 @@ mod tests {
         let bshared = calculates_shared_key(bsk, apk);
         assert_eq!(ashared, bshared);
     }
+    fn generate_group() -> (BigUint, BigUint) {
+        let mut rng = rand::prelude::thread_rng();
+        let g = rng.gen_biguint(1000);
+        let q = rng.gen_biguint(1000);
+        // let g = 10000.to_biguint().unwrap();
+        // let q = 1156651.to_biguint().unwrap();
+        (g, q)
+    }
+    
+    fn diff_key_gen(g: BigUint, q: BigUint) -> (SK, PK) {
+        let sk = generate_private_key(&q);
+        let gd = DiffInt::from(g);
+        let qd = DiffInt::from(q.clone());
+        (sk, calculate_pub_key(gd, qd, sk))
+    }
+    
+    fn generate_private_key(q: &BigUint) -> SK {
+        let mut rng = rand::prelude::thread_rng();
+        let sk = rng.gen_biguint_range(&One::one(), &q);
+        // let sk = 1315000.to_biguint().unwrap();
+        let asf = DiffInt::from(sk);
+        asf
+    }
+    
 }
