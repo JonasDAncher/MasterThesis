a1493184ce1ac4a6f3ee346bd21aae4d96e9e4c9
jonasdancher <jonasdancher@hotmail.com>
2024-04-17 15:39:43 +0200

    now using U128 for type

--- ElGamal/coq_src/El_Gamal.v
+++ ElGamal/coq_src/El_Gamal.v
@@ -9,8 +9,6 @@ Open Scope bool_scope.
 Open Scope hacspec_scope.
 Require Import Hacspec_Lib.
 
-Definition el_int_t := nseq (uint8) (usize 2048).
-
 Definition q_v : int128 :=
   @repr WORDSIZE128 29.
 
@@ -18,76 +16,68 @@ Definition g_v : int128 :=
   @repr WORDSIZE128 2.
 
 Definition enc_aux
-  (source_sk_0 : el_int_t)
+  (secret_source_sk_0 : uint128)
   (target_pk_1 : int128)
   (m_2 : int128)
   
   : (int128 '× int128) :=
   let secret_target_pk_3 : uint128 :=
     uint128_classify (target_pk_1) in 
-  let secret_source_sk_4 : uint128 :=
-    uint128_from_be_bytes (array_from_seq (16) (array_to_seq (source_sk_0))) in 
-  let secret_g_5 : uint128 :=
+  let secret_g_4 : uint128 :=
     uint128_classify (g_v) in 
-  let secret_m_6 : uint128 :=
+  let secret_m_5 : uint128 :=
     uint128_classify (m_2) in 
-  let secret_q_7 : uint128 :=
+  let secret_q_6 : uint128 :=
     uint128_classify (q_v) in 
-  let secret_s_8 : uint128 :=
-    uint128_pow_mod (secret_target_pk_3) (secret_source_sk_4) (secret_q_7) in 
-  let secret_c1_9 : uint128 :=
-    uint128_pow_mod (secret_g_5) (secret_source_sk_4) (secret_q_7) in 
-  let secret_c2_10 : uint128 :=
-    uint128_modulo ((secret_m_6) .* (secret_s_8)) (secret_q_7) in 
-  (uint128_declassify (secret_c1_9), uint128_declassify (secret_c2_10)).
+  let secret_s_7 : uint128 :=
+    uint128_pow_mod (secret_target_pk_3) (secret_source_sk_0) (secret_q_6) in 
+  let secret_c1_8 : uint128 :=
+    uint128_pow_mod (secret_g_4) (secret_source_sk_0) (secret_q_6) in 
+  let secret_c2_9 : uint128 :=
+    uint128_modulo ((secret_m_5) .* (secret_s_7)) (secret_q_6) in 
+  (uint128_declassify (secret_c1_8), uint128_declassify (secret_c2_9)).
 
-Definition keygen   : (int128 '× el_int_t) :=
-  let secret_sk_11 : uint128 :=
+Definition keygen   : (int128 '× uint128) :=
+  let secret_sk_10 : uint128 :=
     uint128_classify (@repr WORDSIZE128 4) in 
-  let secret_g_12 : uint128 :=
+  let secret_g_11 : uint128 :=
     uint128_classify (g_v) in 
-  let secret_q_13 : uint128 :=
+  let secret_q_12 : uint128 :=
     uint128_classify (q_v) in 
-  let pk_14 : int128 :=
-    uint128_declassify (uint128_pow_mod (secret_g_12) (secret_sk_11) (
-        secret_q_13)) in 
-  let sk_15 : el_int_t :=
-    array_from_seq (2048) (array_to_seq (uint128_to_be_bytes (
-        secret_sk_11))) in 
-  (pk_14, sk_15).
+  let pk_13 : int128 :=
+    uint128_declassify (uint128_pow_mod (secret_g_11) (secret_sk_10) (
+        secret_q_12)) in 
+  (pk_13, secret_sk_10).
 
-Definition enc (target_pk_16 : int128) (m_17 : int128)  : (int128 '× int128) :=
-  let '(gy_18, y_19) :=
+Definition enc (target_pk_14 : int128) (m_15 : int128)  : (int128 '× int128) :=
+  let '(gy_16, y_17) :=
     keygen  in 
-  enc_aux (y_19) (target_pk_16) (m_17).
+  enc_aux (y_17) (target_pk_14) (m_15).
 
 Definition dec
-  (target_sk_20 : el_int_t)
-  (c_21 : (int128 '× int128))
+  (secret_target_sk_18 : uint128)
+  (c_19 : (int128 '× int128))
   
   : int128 :=
-  let secret_q_22 : uint128 :=
+  let secret_q_20 : uint128 :=
     uint128_classify (q_v) in 
-  let secret_g_23 : uint128 :=
+  let secret_g_21 : uint128 :=
     uint128_classify (g_v) in 
-  let '(c1_24, c2_25) :=
-    c_21 in 
-  let secret_target_sk_26 : uint128 :=
-    uint128_from_be_bytes (array_from_seq (16) (
-        array_to_seq (target_sk_20))) in 
-  let secret_c1_27 : uint128 :=
-    uint128_classify (c1_24) in 
-  let secret_c2_28 : uint128 :=
-    uint128_classify (c2_25) in 
-  let secret_s_29 : uint128 :=
-    uint128_pow_mod (secret_c1_27) (secret_target_sk_26) (secret_q_22) in 
-  let secret_s_inverse_30 : uint128 :=
-    uint128_pow_mod (secret_s_29) ((secret_q_22) .- (secret_g_23)) (
-      secret_q_22) in 
-  let secret_m_31 : uint128 :=
-    uint128_pow_mod ((secret_c2_28) .* (secret_s_inverse_30)) (
-      uint128_classify (@repr WORDSIZE128 1)) (secret_q_22) in 
-  let m_32 : int128 :=
-    uint128_declassify (secret_m_31) in 
-  m_32.
+  let '(c1_22, c2_23) :=
+    c_19 in 
+  let secret_c1_24 : uint128 :=
+    uint128_classify (c1_22) in 
+  let secret_c2_25 : uint128 :=
+    uint128_classify (c2_23) in 
+  let secret_s_26 : uint128 :=
+    uint128_pow_mod (secret_c1_24) (secret_target_sk_18) (secret_q_20) in 
+  let secret_s_inverse_27 : uint128 :=
+    uint128_pow_mod (secret_s_26) ((secret_q_20) .- (secret_g_21)) (
+      secret_q_20) in 
+  let secret_m_28 : uint128 :=
+    uint128_pow_mod ((secret_c2_25) .* (secret_s_inverse_27)) (
+      uint128_classify (@repr WORDSIZE128 1)) (secret_q_20) in 
+  let m_29 : int128 :=
+    uint128_declassify (secret_m_28) in 
+  m_29.
 
--- ElGamal/rs_src/elgamal.rs
+++ ElGamal/rs_src/elgamal.rs
@@ -1,13 +1,10 @@
 use hacspec_lib::*;
 
-bytes!(ElInt, 16);
-
 const Q : u128 = 29u128;
 const G : u128 = 2u128;
     
-pub fn enc_aux(source_sk: ElInt, target_pk: u128, m: u128) -> (u128, u128) {
+pub fn enc_aux(secret_source_sk: U128, target_pk: u128, m: u128) -> (u128, u128) {
     let secret_target_pk = U128::classify(target_pk);
-    let secret_source_sk = U128_from_be_bytes(U128Word::from_seq(&source_sk));
     let secret_g 		 = U128::classify(G);
     let secret_m         = U128::classify(m);
 	let secret_q 		 = U128::classify(Q);
@@ -19,13 +16,12 @@ pub fn enc_aux(source_sk: ElInt, target_pk: u128, m: u128) -> (u128, u128) {
     (secret_c1.declassify(),secret_c2.declassify())
 }
 
-pub fn keygen() -> (u128, ElInt){
+pub fn keygen() -> (u128, U128){
 	let secret_sk = U128::classify(4u128);
 	let secret_g  = U128::classify(G);
 	let secret_q  = U128::classify(Q);
     let pk 		  = (secret_g.pow_mod(secret_sk,secret_q)).declassify();
-	let sk 		  = ElInt::from_seq(&U128_to_be_bytes(secret_sk));
-    (pk,sk)
+    (pk,secret_sk)
 }
 
 pub fn enc(target_pk: u128, m: u128) -> (u128, u128){
@@ -33,11 +29,10 @@ pub fn enc(target_pk: u128, m: u128) -> (u128, u128){
     enc_aux(y,target_pk,m)
 }
 
-pub fn dec(target_sk: ElInt, c: (u128,u128)) -> u128 {
+pub fn dec(secret_target_sk: U128, c: (u128,u128)) -> u128 {
 	let secret_q 		 = U128::classify(Q);
 	let secret_g  		 = U128::classify(G);
     let (c1,c2) 		 = c;
-    let secret_target_sk = U128_from_be_bytes(U128Word::from_seq(&target_sk));
     let secret_c1 		 = U128::classify(c1);
     let secret_c2 		 = U128::classify(c2);
 
